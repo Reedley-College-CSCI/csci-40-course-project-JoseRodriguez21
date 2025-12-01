b5ee7abfc894bd06ee7107a523710418f5c3fa28
Jose Rodriguez Muro <0957712@MY.SCCCD.EDU>
2025-12-01 14:32:04 -0800

    Added variables to the class, were information from the user will be stored.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,20 @@ using namespace std;
 
 const int MAX_RECIPES = 15;
 
+class Recipe {
+private:
+    string name;
+    int ingredientCount;
+    string* ingredients;
+
+public: 
+    Recipe() {
+        name = "";
+        ingredientCount = 0;
+        ingredients = nullptr;
+    }
+};
+
 int main() {
     string userName;
     int recipeCount = 0;
